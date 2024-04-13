5ce8accf120f9d40fd5b13e9bd64b32ac62b5b6d
shubham <tonygupta275@gmail.com>
2024-04-13 20:14:50 +0530

    add routes folder

--- server/index.js
+++ server/index.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import Connection from './database/db.js';
 import dotenv from 'dotenv';
-dotenv.config();
-
+import Routes from './routes/route.js';
+import cors from 'cors';
 const app=express();
 const PORT=8000;
+dotenv.config();
+
+app.use(cors());
+app.use('/',Routes);
+
 const username=process.env.USERNAME;
 const password=process.env.PASSWORD;
 Connection(username,password);
